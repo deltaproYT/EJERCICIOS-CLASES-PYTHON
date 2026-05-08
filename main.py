51620272ae10e1cd0aa11fbfa0b564c698c4fa52
hllerenaa <hllerenaa1h@gmail.com>
2026-05-07 20:52:04 -0500

    Practica

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-var1 = 5.5
-print(var1)
-
-
-var2 = int(5.5)
-print(var2)
-
-
-nombre = 'Hector'
-apellidos = 'Llerena'
-edad = 28
-print(f"{nombre} {apellidos}")
-print(f"{nombre} {apellidos} tiene {edad} años")
-
-nombre, apellidos, edad = 'Hector', 'Llerena', 28
-print(f"{nombre} {apellidos} tiene {edad} años")
-
+print(f"Hola Clase")
